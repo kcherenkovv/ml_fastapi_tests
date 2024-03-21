97457677182b9a405301136dc445a0f3342591ae
kcherenkovv <kcherenkov9888@mail.ru>
2024-03-21 19:42:28 +0300

    Add a new method

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
-from transformers import pipeline
+from transformers import pipeline, AutoTokenizer, AutoModelForSeq2SeqLM
 from pydantic import BaseModel
+
 
 
 class Item(BaseModel):
@@ -10,6 +11,10 @@ class Item(BaseModel):
 
 app = FastAPI()
 classifier = pipeline("sentiment-analysis")
+tokenizer = AutoTokenizer.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
+model = AutoModelForSeq2SeqLM.from_pretrained("Helsinki-NLP/opus-mt-en-ru")
+
+
 
 
 
@@ -21,3 +26,11 @@ def root():
 @app.post("/predict/")
 def predict(item: Item):
     return classifier(item.text)[0]
+
+
+@app.post("/translate/")
+def translate_text(item: Item):
+    inputs = tokenizer(item.text, return_tensors="pt")
+    outputs = model.generate(**inputs)
+    translated_text = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return {"translated_text": translated_text}
