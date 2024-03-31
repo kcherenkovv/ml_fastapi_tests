518e30121f1044ed01e593a56e945a8476836e02
Серафим <zagorodniy.serafim@mail.ru>
2024-03-31 07:00:13 +0300

    Update main.py test_main.py

--- test_main.py
+++ test_main.py
@@ -25,8 +25,9 @@ def test_predict_negative():
     assert response.status_code == 200
     assert json_data['label'] == 'NEGATIVE'
 
+
 def test_translate():
     response = client.post("/translate/",
-                          json={"text":"I love machine learning!"})
+                           json={"text": "I love machine learning!"})
     assert response.status_code == 200
     assert response.json()["translated_text"] == "Я люблю машинное обучение!"
